a62a9f31bdb67e77360975af1299b21a27602c90
Sébastien Lucas <sebastien@slucas.fr>
2014-12-11 10:37:50 +0100

    Ajout des labels et du type de retour.

--- src/QD/SuperBundle/Form/Type/DossierType.php
+++ src/QD/SuperBundle/Form/Type/DossierType.php
@@ -4,6 +4,7 @@ namespace QD\SuperBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 /**
  * Description of DossierType
@@ -15,13 +16,22 @@ class DossierType extends AbstractType {
     {
         $builder
             ->add('titre')
-            ->add('sousTitre')
+            ->add('sousTitre', 'text', array('required' => false, 'label' => 'Sous-titre'))
             ->add('contenu', 'textarea')
-            ->add('dateDebut', 'date')
-            ->add('dateFin', 'date')
-            ->add('urlImage', 'url')
+            ->add('dateDebut', 'date', array('label' => 'Date de début'))
+            ->add('dateFin', 'date', array('label' => 'Date de fin'))
+            ->add('urlImage', 'url', array('required' => false, 'label' => "Url de l''image"))
             ->add('tarif', 'money')
-            ->add('save', 'submit');
+            ->add('Sauvegarde', 'submit');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        //parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array('data_class' => 'QD\SuperBundle\Entity\Dossier'));
     }
     
     public function getName() {
